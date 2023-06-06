c75fcddf903494b39aecd0a8f203574d0d5067bf
jjahanip <jahanadar.jahani@gmail.com>
2023-06-06 16:35:58 -0400

    fix bug with run_notebook

--- run_notebook.bat
+++ run_notebook.bat
@@ -4,7 +4,7 @@ REM Activate the desired Conda environment
 call conda activate sam
 
 REM Reset and clear the output of the Jupyter Notebook file
-jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace predictor_example.ipynb
+jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace sam_segmenter.ipynb
 
 REM Open the main Jupyter Notebook file
 jupyter notebook sam_segmenter.ipynb
